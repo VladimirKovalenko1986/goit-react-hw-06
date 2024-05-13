1f21bec4b0d295a2bb6e8da008b9a605169ebfa9
Volodimir Kovalenko <volodimirkovalenko@Volodimirs-MacBook-Pro.local>
2024-05-13 18:03:29 +0300

    fixed searchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,11 +1,11 @@
 import css from "./SearchBox.module.css";
 import { useId } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changeFilter } from "../../redux/filtersSlice";
 
 export default function SearchBox() {
   const sortId = useId();
-
+  const filter = useSelector((state) => state.filters.name);
   const dispatch = useDispatch();
 
   return (
@@ -13,6 +13,7 @@ export default function SearchBox() {
       <label htmlFor={sortId}>Find contacts by name</label>
       <input
         type="text"
+        value={filter}
         className={css.input}
         id={sortId}
         onChange={(e) => dispatch(changeFilter(e.target.value))}
